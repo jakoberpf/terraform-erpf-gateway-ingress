533011de2b63bd3cbbefa37feba29bef7a4c5978
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-02-18 18:26:47 +0100

    remove provider from module

--- main.tf
+++ main.tf
@@ -11,32 +11,6 @@ terraform {
   }
 }
 
-provider "remote" {
-  max_sessions = 2
-
-  conn {
-    user        = "ubuntu"
-    private_key = var.ssh_secret_key
-    host        = "0.gateway.dns.erpf.de"
-    sudo        = true
-  }
-}
-
-# locals {
-#   domain_map = flatten([
-#     for key, value in var.sql_instances : [
-#       for item in value.db_list : {
-#         name     = item
-#         instance = key
-#       }
-#     ]
-#   ])
-#   sql_databases = {
-#     for item in local.databases :
-#     uuid() => item
-#   }
-# }
-
 resource "random_string" "deployment_id" {
   length  = 5
   upper   = false
