2b27e358496993269e8f1e7dd9a5cf54f0417ec4
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-02-24 16:59:57 +0100

    update test module

--- test/terraform/module.tf
+++ test/terraform/module.tf
@@ -1,24 +1,22 @@
-module "this" {
+module "ingress_module" {
   source = "../../"
 
   providers = {
     cloudflare = cloudflare
-    remote     = remote
+    remote     = remote.gateway1
   }
 
   domains = [
-    "test-application.erpf.de"
+    "domain.test.erpf.de"
   ]
   host = "localhost"
   port = "2222"
-
-  ssh_secret_key = file("~/.ssh/id_rsa")
 
   cloudflare_email   = var.cloudflare_email
   cloudflare_zone_id = var.cloudflare_zone_id
   cloudflare_token   = var.cloudflare_token
 
-  cloudflare_record_value = "test" #var.cloudflare_record_value
-  #cloudflare_record_type    = "" #var.cloudflare_record_type
-  cloudflare_record_proxied = false #var.cloudflare_record_proxied
+  cloudflare_record_value   = "gateway.test.erpf.de" # var.cloudflare_record_value
+  cloudflare_record_type    = "CNAME"                # var.cloudflare_record_type
+  cloudflare_record_proxied = false                  # var.cloudflare_record_proxied
 }
