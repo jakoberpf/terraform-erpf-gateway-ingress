68353f3379b4d4478c65423acfe0f713893c8e96
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-04-17 00:13:52 +0200

    update resouces

--- caddy.tf
+++ caddy.tf
@@ -1,20 +1,30 @@
-data "template_file" "this" {
+data "template_file" "default" {
   count    = length(var.domains)
-  template = file("${path.module}/templates/caddy.tftpl")
+  template = file("${path.module}/templates/caddy.default.tpl")
 
   vars = {
     domain           = var.domains[count.index]
     host             = var.host
     port             = var.port
-    cloudflare_email = var.cloudflare_email
-    cloudflare_token = var.cloudflare_token
     log_file_path    = "/var/log/caddy/access-${replace(var.domains[count.index], ".", "-")}.log"
   }
 }
 
+# data "template_file" "k8s" {
+#   count    = length(var.domains)
+#   template = file("${path.module}/templates/caddy.default.tpl")
+
+#   vars = {
+#     domain           = var.domains[count.index]
+#     host             = var.host
+#     port             = var.port
+#     log_file_path    = "/var/log/caddy/access-${replace(var.domains[count.index], ".", "-")}.log"
+#   }
+# }
+
 resource "remote_file" "this" {
   count       = length(var.domains)
   path        = "/home/ubuntu/caddy/${replace(var.domains[count.index], ".", "-")}.caddy"
-  content     = data.template_file.this[count.index].rendered
+  content     = data.template_file.default[count.index].rendered
   permissions = "0644"
 }
